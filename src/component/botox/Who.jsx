9bd7e1d9a454ca0b4a67ceafd0b52946343d52d2
enzoaf99 <enzoafernandez99@gmail.com>
2024-01-27 08:42:05 -0300

    Changes who

--- src/component/botox/Who.jsx
+++ src/component/botox/Who.jsx
@@ -7,50 +7,35 @@ function Who() {
                 <h2 className={"text-white fw-bold text-center m-0"}>
                     ¿PARA QUIEN ESTÁ DIRIGIDO EL CURSO?
                 </h2>
+                <p className={"text-white fw-light pt-1 text-center"}>
+                    En el curso aprenderemos a APLICAR BOTOX de DOS formas distintas
+                </p>
             </div>
 
             <div className={"container pt-5"}>
-                <div className={"row"}>
+                <div className={"row justify-content-center"}>
                     <div className={"col-md-6 col-xl-4 col-xxl-4 pb-5"}>
                         <div
                             className={"container text-white text-center pt-2 ps-5 pe-5 pb-2 bg-jaim-primary-gradient rounded-2 card-who"}>
-                            <Image src="/images/good.png" alt="Good" width={90} height={90} />
-                            <h3>
-                                PERSONAS QUE SON PRINCIPIANTES EN EL CAMPO
+                            <Image src="/images/good.png" alt="Good" width={90} height={90}/>
+                            <h3 className={"pt-3"}>
+                                INYECTABLE
                             </h3>
-                            <p>
-                                Aprenderas toda la teoría, desde los conceptos básicos del botox y Dermapen hasta los
-                                conceptos avanzados de aplicación.
+                            <p className={"pb-5"}>
+                                Para personal habilitado, por ejemplo: Médicos, Cirujanos, Odontólogos
                             </p>
                         </div>
                     </div>
 
-                    <div className={"col-md-6 col-xl-4 col-xxl-4 pb-3"}>
+                    <div className={"col-md-6 col-xl-4 col-xxl-4 pb-5"}>
                         <div
                             className={"container text-white text-center pt-2 ps-5 pe-5 pb-2 bg-jaim-primary-gradient rounded-2 card-who"}>
-                            <Image src="/images/good.png" alt="Good" width={90} height={90} />
-                            <h3>
-                                PROFESIONALES QUE YA TRABAJAN EN EL CAMPO
+                            <Image src="/images/good.png" alt="Good" width={90} height={90}/>
+                            <h3 className={"pt-3"}>
+                                DERMAPEN
                             </h3>
-                            <p>
-                                No basta con ser micropigmentador y no saber realizar las técnicas correctamente, por
-                                eso si quieres mejorar y dominar las técnicas de Hilos en Tebori y Micropigmentación de
-                                Labios, entonces ¡SÍ! Este curso también es para ti
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className={"col-md-6 col-xl-4 col-xxl-4 pb-3"}>
-                        <div
-                            className={"container text-white text-center pt-2 ps-5 pe-5 pb-2 bg-jaim-primary-gradient rounded-2 card-who"}>
-                            <Image src="/images/good.png" alt="Good" width={90} height={90} />
-                            <h3>
-                                PROFESIONALES QUE YA TRABAJAN EN EL CAMPO
-                            </h3>
-                            <p>
-                                No basta con ser micropigmentador y no saber realizar las técnicas correctamente, por
-                                eso si quieres mejorar y dominar las técnicas de Hilos en Tebori y Micropigmentación de
-                                Labios, entonces ¡SÍ! Este curso también es para ti
+                            <p className={"pb-5"}>
+                                Apto para cosmetologas, cosmiatras, esteticistas, dermatocosmiatras, mesoterapeutas, enfermeros y mucho más!
                             </p>
                         </div>
                     </div>
