84e25ef5a6c705c2088a32c512ecdf6bd7dedd38
enzoaf99 <enzoafernandez99@gmail.com>
2024-02-01 11:38:11 -0300

    Init checkout

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,12 +1,122 @@
 "use client"
+import {useState} from 'react';
+import Image from 'next/image';
+import "../shop/style.css"
 
 export default function Shop() {
+    const [selectedCountry, setSelectedCountry] = useState(null);
+
+    const handleCountryClick = (country) => {
+        setSelectedCountry(country);
+    };
+
     return (
         <>
-            <div className={"container"}>
-                <h3>Shop</h3>
+            <div className={'container pt-4'}>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Checkout</h5>
+                        <h6 className="card-subtitle mb-2 text-body-secondary">
+                            Haga click en su país para comenzar
+                        </h6>
+                        <div className={'row justify-content-center'}>
+                            <div className={"col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 pt-4"}>
+                                <div className={"d-flex justify-content-around"}>
+                                    <a
+                                        href="#"
+                                        onClick={() => handleCountryClick('arg')}
+                                        className={selectedCountry === 'arg' ? 'selected' : ''}
+                                    >
+                                        <Image src={'/images/country/arg.jpg'} alt={'Arg'} width={'45'} height={'30'}/>
+                                    </a>
 
+                                    <a
+                                        href="#"
+                                        onClick={() => handleCountryClick('chl')}
+                                        className={selectedCountry === 'chl' ? 'selected' : ''}
+                                    >
+                                        <Image src={'/images/country/chile.png'} alt={'Chl'} width={'45'}
+                                               height={'30'}/>
+                                    </a>
+
+                                    <a
+                                        href="#"
+                                        onClick={() => handleCountryClick('col')}
+                                        className={selectedCountry === 'col' ? 'selected' : ''}
+                                    >
+                                        <Image src={'/images/country/col.png'} alt={'Col'} width={'45'} height={'30'}/>
+                                    </a>
+
+                                    <a
+                                        href="#"
+                                        onClick={() => handleCountryClick('eeuu')}
+                                        className={selectedCountry === 'eeuu' ? 'selected' : ''}
+                                    >
+                                        <Image src={'/images/country/eeuu.webp'} alt={'EEUU'} width={'45'}
+                                               height={'30'}/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className={"col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 pt-4"}>
+                                <div className={"d-flex justify-content-around"}>
+                                    <a
+                                        href="#"
+                                        onClick={() => handleCountryClick('mex')}
+                                        className={selectedCountry === 'mex' ? 'selected' : ''}
+                                    >
+                                        <Image src={'/images/country/mex.png'} alt={'Mex'} width={'45'} height={'30'}/>
+                                    </a>
+
+                                    <a
+                                        href="#"
+                                        onClick={() => handleCountryClick('par')}
+                                        className={selectedCountry === 'par' ? 'selected' : ''}
+                                    >
+                                        <Image src={'/images/country/par.png'} alt={'Par'} width={'45'} height={'30'}/>
+                                    </a>
+
+                                    <a
+                                        href="#"
+                                        onClick={() => handleCountryClick('pr')}
+                                        className={selectedCountry === 'pr' ? 'selected' : ''}
+                                    >
+                                        <Image src={'/images/country/pr.png'} alt={'Puerto Rico'} width={'45'}
+                                               height={'30'}/>
+                                    </a>
+
+                                    <a
+                                        href="#"
+                                        onClick={() => handleCountryClick('otro')}
+                                        className={selectedCountry === 'otro' ? 'selected' : ''}
+                                    >
+                                        <Image src={'/images/country/otro.png'} alt={'Otro'} width={'45'}
+                                               height={'30'}/>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={"container pt-4"}>
+                            <p style={{display: selectedCountry ? 'block' : 'none'}}>Paypal</p>
+                            <p style={{display: selectedCountry === 'arg' ? 'block' : 'none'}}>Mercado Pago</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            <style jsx>{`
+                a {
+                    filter: grayscale(${selectedCountry ? '100%' : '0%'});
+                }
+
+                a.selected {
+                    filter: grayscale(0%); /* Elimina la escala de grises para la bandera seleccionada */
+                }
+
+                p:last-child {
+                    display: ${selectedCountry === 'arg' ? 'block' : 'none'};
+                }
+            `}</style>
         </>
-    )
+    );
 }
