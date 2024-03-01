4583c074a4b452f204c45e119c3385989c3f0543
enzoaf99 <enzoafernandez99@gmail.com>
2024-03-01 17:55:08 -0300

    prod fixes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@
  */
 const nextConfig = {
     output: 'export',
-
+    assetPrefix: 'https://jaim.online/', // Esta es la URL base donde se sirven tus archivos estáticos
+    crossOrigin: "anonymous"
 }
 
 module.exports = nextConfig
